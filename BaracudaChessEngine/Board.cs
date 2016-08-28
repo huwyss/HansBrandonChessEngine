0140d647531098e30cf2bf150f59e8ef4e09111f
huwyss <hansueli.wyss@gmail.com>
2016-08-28 15:22:28 +0200

    Started Board implementation.

---
+++ BaracudaChessEngine/Board.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaracudaChessEngine
+{
+    public class Board
+    {
+        char[] _board;
+        string initPosition = "RNBQKBNR" + // zeile 1 weiss
+                              "PPPPPPPP" +
+                              "        " +
+                              "        " +
+                              "        " +
+                              "        " +
+                              "pppppppp" +
+                              "rnbqkbnr"; // zeile 8 schwarz
+
+        /// <summary>
+        /// Constructor. Board is empty.
+        /// </summary>
+        public Board()
+        {
+            _board = new char[64];
+            for (int i = 0; i < 64; i++)
+            {
+                _board[i] = ' ';
+            }
+        }
+
+        /// <summary>
+        /// Sets the initial chess position.
+        /// </summary>
+        public void  SetInitialPosition()
+        {
+            for (int i = 0; i < 64; i++)
+            {
+                _board[i] = initPosition[i];
+            }
+        }
+
+        /// <summary>
+        /// Returns the chess piece.
+        /// </summary>
+        /// <param name="file">1 to 8</param>
+        /// <param name="rank">1 to 8</param>
+        /// <returns></returns>
+        public char GetPiece(int file, int rank)
+        {
+            return _board[8 * (rank - 1) + file - 1];
+        }
+
+        /// <summary>
+        /// Returns the chess piece.
+        /// </summary>
+        /// <param name="fileChar">'a' to 'h'</param>
+        /// <param name="rank">1 to 8</param>
+        /// <returns></returns>
+        public char GetPiece(char fileChar, int rank)
+        {
+            return GetPiece(fileChar - 'a' + 1, rank);
+        }
+
+        /// <summary>
+        /// Sets a chess piece to the field.
+        /// </summary>
+        /// <param name="piece">chess piece: p-pawn, k-king, q-queen, r-rook, n-knight, b-bishop, r-rook, ' '-empty. small=black, capital=white</param>
+        /// <param name="file">1 to 8</param>
+        /// <param name="rank">1 to 8</param>
+        public void SetPiece(char piece, int file, int rank)
+        {
+            _board[8 * (rank - 1) + file - 1] = piece;
+        }
+
+        /// <summary>
+        /// Sets a chess piece to the field.
+        /// </summary>
+        /// <param name="piece">chess piece: p-pawn, k-king, q-queen, r-rook, n-knight, b-bishop, r-rook, ' '-empty. small=black, capital=white</param>
+        /// <param name="file">'a' to 'h'</param>
+        /// <param name="rank">1 to 8</param>
+        public void SetPiece(char piece, char fileChar, int rank)
+        {
+            SetPiece(piece, fileChar - 'a' + 1, rank);
+        }
+
+
+    }
+}
---
+++ BaracudaChessEngineTest/BoardTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BaracudaChessEngine;
+
+namespace BaracudaChessEngineTest
+{
+    [TestClass]
+    public class BoardTest
+    {
+        [TestMethod]
+        public void GetSetPieceTest_WhenSetPieceRookToD8_ThenGetPieceD8ShouldReturnRook()
+        {
+            var target = new Board();
+            target.SetPiece('R', 4, 8);
+            char piece = target.GetPiece('d', 8);
+            Assert.AreEqual('R', piece);
+        }
+
+        [TestMethod]
+        public void GetSetPieceTest_WhenSetPieceRookTo48_ThenGetPiece48ShouldReturnRook()
+        {
+            var target = new Board();
+            target.SetPiece('R', 4, 8);
+            char piece = target.GetPiece(4, 8);
+            Assert.AreEqual('R', piece);
+        }
+
+        [TestMethod]
+        public void GetPiece_WhenNewBoard_ThenAllPositionsEmpty()
+        {
+            var target = new Board();
+            char piece = target.GetPiece('d', 8);
+            Assert.AreEqual(' ', piece);
+        }
+
+        [TestMethod]
+        public void InitPosition_WhenInitializedPosition_ThenPiecesAtInitPosition()
+        {
+            var target = new Board();
+            target.SetInitialPosition();
+
+            Assert.AreEqual('R', target.GetPiece('a', 1));
+            Assert.AreEqual('N', target.GetPiece('b', 1));
+            Assert.AreEqual('B', target.GetPiece('c', 1));
+            Assert.AreEqual('Q', target.GetPiece('d', 1));
+            Assert.AreEqual('K', target.GetPiece('e', 1));
+            Assert.AreEqual('B', target.GetPiece('f', 1));
+            Assert.AreEqual('N', target.GetPiece('g', 1));
+            Assert.AreEqual('R', target.GetPiece('h', 1));
+
+            Assert.AreEqual('P', target.GetPiece('b', 2)); // white pawn
+            Assert.AreEqual(' ', target.GetPiece('c', 3)); // empty
+            Assert.AreEqual(' ', target.GetPiece('d', 4)); // empty
+            Assert.AreEqual(' ', target.GetPiece('e', 5)); // empty
+            Assert.AreEqual(' ', target.GetPiece('f', 6)); // empty
+            Assert.AreEqual('p', target.GetPiece('g', 7)); // black pawn
+
+            Assert.AreEqual('r', target.GetPiece('a', 8));
+            Assert.AreEqual('n', target.GetPiece('b', 8));
+            Assert.AreEqual('b', target.GetPiece('c', 8));
+            Assert.AreEqual('q', target.GetPiece('d', 8));
+            Assert.AreEqual('k', target.GetPiece('e', 8));
+            Assert.AreEqual('b', target.GetPiece('f', 8));
+            Assert.AreEqual('n', target.GetPiece('g', 8));
+            Assert.AreEqual('r', target.GetPiece('h', 8));
+        }
+
+    }
+}

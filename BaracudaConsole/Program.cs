935387c618c8a4beb7934e6dd908235047ca46fa
huwyss <hansueli.wyss@gmail.com>
2017-02-06 15:44:57 +0100

    can create statistics engine vs engine.

--- BaracudaConsole/Program.cs
+++ BaracudaConsole/Program.cs
@@ -11,127 +11,115 @@ namespace BaracudaConsole
     {
         static void Main(string[] args)
         {
-            bool whiteHuman = true;
+            bool whiteHuman = false;
             bool blackHuman = false;
 
-            bool showBoard = true;
+            int runStatisticGames = 100; // run statistic = 1 --> only one game
+            bool quiet = true; // quiet = true --> no display of moves or board
 
             BaracudaEngine _whiteEngine = new BaracudaEngine();
             BaracudaEngine _blackEngine = new BaracudaEngine();
 
-            int moveCount = 1;
-            _whiteEngine.SetInitialPosition();
-            _blackEngine.SetInitialPosition();
+            int whiteWins = 0;
 
             bool isMoveValid;
 
-            if (showBoard)
+            if (!quiet)
             {
                 PrintBoard(_whiteEngine);
             }
 
-            while (true)
+            for (int i = 0; i < runStatisticGames; i++)
             {
-                if (_whiteEngine.SideToMove() == Definitions.ChessColor.White)
-                {
-                    Console.WriteLine(moveCount++);
-                }
+                int moveCount = 1;
+                _whiteEngine.SetInitialPosition();
+                _blackEngine.SetInitialPosition();
 
-                Console.WriteLine("Next Move: " + _whiteEngine.SideToMove());
-
-                // Human move
-                if ((whiteHuman && _whiteEngine.SideToMove() == Definitions.ChessColor.White) ||
-                    (blackHuman && _whiteEngine.SideToMove() == Definitions.ChessColor.Black))
+                while (true)
                 {
-                    // human move from console
-                    do
+                    if (!quiet)
                     {
-                        Console.WriteLine("Enter your move (ie. e2e4): ");
-                        string moveConsoleString = Console.ReadLine();
-                        moveConsoleString = moveConsoleString.Trim();
-                        isMoveValid = _whiteEngine.Move(moveConsoleString);
-                        if (!isMoveValid)
+                        if (_whiteEngine.SideToMove() == Definitions.ChessColor.White)
                         {
-                            Console.WriteLine("Invalid move.");
+                            Console.WriteLine(moveCount++);
                         }
-                        else
+                    }
+
+                    if (!quiet)
+                    {
+                        Console.WriteLine("Next Move: " + _whiteEngine.SideToMove());
+                    }
+
+                    // Human move
+                    if ((whiteHuman && _whiteEngine.SideToMove() == Definitions.ChessColor.White) ||
+                        (blackHuman && _whiteEngine.SideToMove() == Definitions.ChessColor.Black))
+                    {
+                        // human move from console
+                        do
                         {
-                            _blackEngine.Move(moveConsoleString);
+                            Console.WriteLine("Enter your move (ie. e2e4): ");
+                            string moveConsoleString = Console.ReadLine();
+                            moveConsoleString = moveConsoleString.Trim();
+                            isMoveValid = _whiteEngine.Move(moveConsoleString);
+                            if (!isMoveValid)
+                            {
+                                Console.WriteLine("Invalid move.");
+                            }
+                            else
+                            {
+                                _blackEngine.Move(moveConsoleString);
+                            }
+                        } while (!isMoveValid);
+                    }
+                    else
+                    {
+                        Move moveComputer = null;
+                        // computer move for white
+                        if (!whiteHuman && _whiteEngine.SideToMove() == Definitions.ChessColor.White)
+                        {
+                            moveComputer = _whiteEngine.DoBestMove(Definitions.ChessColor.White);
+                            _blackEngine.Move(moveComputer);
                         }
-                    } while (!isMoveValid);
-                }
-                else
-                {
-                    Move moveComputer = null;
-                    // computer move for white
-                    if (!whiteHuman && _whiteEngine.SideToMove() == Definitions.ChessColor.White)
-                    {
-                        moveComputer = _whiteEngine.DoBestMove(Definitions.ChessColor.White);
-                        _blackEngine.Move(moveComputer);
-                    }
-                    // computer move for black
-                    else if (!blackHuman && _whiteEngine.SideToMove() == Definitions.ChessColor.Black)
-                    {
-                        moveComputer = _blackEngine.DoBestMove(Definitions.ChessColor.Black);
-                        _whiteEngine.Move(moveComputer);
+                        // computer move for black
+                        else if (!blackHuman && _whiteEngine.SideToMove() == Definitions.ChessColor.Black)
+                        {
+                            moveComputer = _blackEngine.DoBestMove(Definitions.ChessColor.Black);
+                            _whiteEngine.Move(moveComputer);
+                        }
+
+                        if (!quiet && moveComputer.ToString() != "")
+                        {
+                            Console.WriteLine("Computer move: " + moveComputer.ToString());
+                        }
                     }
 
-                    if (moveComputer.ToString() != "")
+
+                    if (!quiet)
+                        PrintBoard(_whiteEngine);
+
+                    if (_whiteEngine.IsWinner(Definitions.ChessColor.White))
                     {
-                        Console.WriteLine("Computer move: " + moveComputer.ToString());
+                        if (!quiet)
+                            Console.WriteLine("\nWhite wins!");
+
+                        whiteWins++;
+                        break;
+                    }
+                    if (_whiteEngine.IsWinner(Definitions.ChessColor.Black))
+                    {
+                        if (!quiet)
+                            Console.WriteLine("\nBlack wins!");
+
+                        break;
                     }
                 }
 
-                if (showBoard)
-                {
-                    PrintBoard(_whiteEngine);
-                }
-
-                if (_whiteEngine.IsWinner(Definitions.ChessColor.White))
-                {
-                    Console.WriteLine("\nWhite wins!");
-                    break;
-                }
-                if (_whiteEngine.IsWinner(Definitions.ChessColor.Black))
-                {
-                    Console.WriteLine("\nBlack wins!");
-                    break;
-                }
+                Console.WriteLine("Games: " + i + " - White wins: " + whiteWins);
             }
 
+            Console.WriteLine("\n\nResult\n\nGames: " + runStatisticGames + " - White wins: " + whiteWins);
             Console.ReadLine();
         }
-
-
-
-
-
-        //PrintBoard(_engine);
-        //        if (_engine.IsWinner(Definitions.ChessColor.White))
-        //        {
-        //            Console.WriteLine("\nWhite wins!");
-        //            break;
-        //        }
-
-        //        Move moveBlack = _engine.DoBestMove(Definitions.ChessColor.Black);
-        //        if (moveBlack.ToString() != "")
-        //        {
-        //            Console.WriteLine("Computer move: " + moveBlack.ToString());
-        //            PrintBoard(_engine);
-        //            if (_engine.IsWinner(Definitions.ChessColor.Black))
-        //            {
-        //                Console.WriteLine("\nComputer wins!");
-        //                break;
-        //            }
-        //        }
-        //        else
-        //        {
-        //            Console.WriteLine("Computer gives up. You win!");
-        //        }
-        //    }
-
-        //    Console.ReadLine();
-        //}
 
         private static void PrintBoard(BaracudaEngine engine)
         {

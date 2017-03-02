06471651ac3943fc006ffb12c051e6ef94e1081a
huwyss <hansueli.wyss@gmail.com>
2017-03-02 14:00:36 +0100

    removed duplication in CastlingMove class.

--- MantaChessEngine/Move/CastlingMove.cs
+++ MantaChessEngine/Move/CastlingMove.cs
@@ -18,6 +18,12 @@ namespace MantaChessEngine
     {
         private CastlingType _castlingType;
 
+        char _rook = (char)0;
+        int _rookOriginalFile = 0;
+        int _rookOriginalRank = 0;
+        int _rookCastledFile = 0;
+        int _rookCastledRank = 0;
+
         public CastlingMove(char movingPiece, int sourceFile, int sourceRank, int targetFile, int targetRank, CastlingType castlingType)
             : base(movingPiece, sourceFile, sourceRank, targetFile, targetRank, Definitions.EmptyField, false)
         {
@@ -28,31 +34,30 @@ namespace MantaChessEngine
         {
             if (_castlingType == CastlingType.WhiteKingSide)
             {
-                board.SetPiece(Definitions.ROOK.ToString().ToUpper()[0], Helper.FileCharToFile('f'), 1); // move rook next to king
-                board.SetPiece(Definitions.EmptyField, Helper.FileCharToFile('h'), 1); // remove old rook
+                SetWhiteKingSideRook();
                 board.WhiteDidCastling = true;
             }
 
             if (_castlingType == CastlingType.WhiteQueenSide)
             {
-                board.SetPiece(Definitions.ROOK.ToString().ToUpper()[0], Helper.FileCharToFile('d'), 1); // move rook next to king
-                board.SetPiece(Definitions.EmptyField, Helper.FileCharToFile('a'), 1); // remove old rook
+                SetWhiteQueenSideRook();
                 board.WhiteDidCastling = true;
             }
 
             if (_castlingType == CastlingType.BlackKingSide)
             {
-                board.SetPiece(Definitions.ROOK.ToString().ToLower()[0], Helper.FileCharToFile('f'), 8); // move rook next to king
-                board.SetPiece(Definitions.EmptyField, Helper.FileCharToFile('h'), 8); // remove old rook
+                SetBlackKingSideRook();
                 board.BlackDidCastling = true;
             }
 
             if (_castlingType == CastlingType.BlackQueenSide)
             {
-                board.SetPiece(Definitions.ROOK.ToString().ToLower()[0], Helper.FileCharToFile('d'), 8); // move rook next to king
-                board.SetPiece(Definitions.EmptyField, Helper.FileCharToFile('a'), 8); // remove old rook
+                SetBlackQueenSideRook();
                 board.BlackDidCastling = true;
             }
+
+            board.SetPiece(_rook, _rookCastledFile, _rookCastledRank); // move rook next to king
+            board.SetPiece(Definitions.EmptyField, _rookOriginalFile, _rookOriginalRank); // remove old rook
 
             base.ExecuteMove(board);
         }
@@ -61,33 +66,68 @@ namespace MantaChessEngine
         {
             if (_castlingType == CastlingType.WhiteKingSide)
             {
-                board.SetPiece(Definitions.ROOK.ToString().ToUpper()[0], Helper.FileCharToFile('h'), 1); // move rook next to king
-                board.SetPiece(Definitions.EmptyField, Helper.FileCharToFile('f'), 1); // remove old rook
+                SetWhiteKingSideRook();
                 board.WhiteDidCastling = false;
             }
 
             if (_castlingType == CastlingType.WhiteQueenSide)
             {
-                board.SetPiece(Definitions.ROOK.ToString().ToUpper()[0], Helper.FileCharToFile('a'), 1); // move rook next to king
-                board.SetPiece(Definitions.EmptyField, Helper.FileCharToFile('d'), 1); // remove old rook
+                SetWhiteQueenSideRook();
                 board.WhiteDidCastling = false;
             }
 
             if (_castlingType == CastlingType.BlackKingSide)
             {
-                board.SetPiece(Definitions.ROOK.ToString().ToLower()[0], Helper.FileCharToFile('h'), 8); // move rook next to king
-                board.SetPiece(Definitions.EmptyField, Helper.FileCharToFile('f'), 8); // remove old rook
+                SetBlackKingSideRook();
                 board.BlackDidCastling = false;
             }
 
             if (_castlingType == CastlingType.BlackQueenSide)
             {
-                board.SetPiece(Definitions.ROOK.ToString().ToLower()[0], Helper.FileCharToFile('a'), 8); // move rook next to king
-                board.SetPiece(Definitions.EmptyField, Helper.FileCharToFile('d'), 8); // remove old rook
+                SetBlackQueenSideRook();
                 board.BlackDidCastling = false;
             }
 
+            board.SetPiece(_rook, _rookOriginalFile, _rookOriginalRank); // move rook next to king
+            board.SetPiece(Definitions.EmptyField, _rookCastledFile, _rookCastledRank); // remove old rook
+
             base.UndoMove(board);
+        }
+
+        private void SetWhiteKingSideRook()
+        {
+            _rook = Definitions.ROOK.ToString().ToUpper()[0];
+            _rookOriginalFile = Helper.FileCharToFile('h');
+            _rookOriginalRank = 1;
+            _rookCastledFile = Helper.FileCharToFile('f');
+            _rookCastledRank = 1;
+        }
+
+        private void SetWhiteQueenSideRook()
+        {
+            _rook = Definitions.ROOK.ToString().ToUpper()[0];
+            _rookOriginalFile = Helper.FileCharToFile('a');
+            _rookOriginalRank = 1;
+            _rookCastledFile = Helper.FileCharToFile('d');
+            _rookCastledRank = 1;
+        }
+
+        private void SetBlackKingSideRook()
+        {
+            _rook = Definitions.ROOK.ToString().ToLower()[0];
+            _rookOriginalFile = Helper.FileCharToFile('h');
+            _rookOriginalRank = 8;
+            _rookCastledFile = Helper.FileCharToFile('f');
+            _rookCastledRank = 8;
+        }
+
+        private void SetBlackQueenSideRook()
+        {
+            _rook = Definitions.ROOK.ToString().ToLower()[0];
+            _rookOriginalFile = Helper.FileCharToFile('a');
+            _rookOriginalRank = 8;
+            _rookCastledFile = Helper.FileCharToFile('d');
+            _rookCastledRank = 8;
         }
     }
 }

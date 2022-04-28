1b866992fe665d644541df2f0534ebab7406b981
huwyss <hansueli.wyss@gmail.com>
2022-04-28 20:58:31 +0200

    Started with a Bitboard.

---
+++ MantaChessEngine/Bitboard/Bitboards.cs
@@ -0,0 +1,426 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bitboard = System.UInt64;
+
+
+namespace MantaChessEngine
+{
+    public class Chess
+    {
+        public const int A1 = 0;
+        public const int B1 = 1;
+        public const int C1 = 2;
+        public const int D1 = 3;
+        public const int E1 = 4;
+        public const int F1 = 5;
+        public const int G1 = 6;
+        public const int H1 = 7;
+
+        public const int A2 = 8;
+        public const int B2 = 9;
+        public const int C2 = 10;
+        public const int D2 = 11;
+        public const int E2 = 12;
+        public const int F2 = 13;
+        public const int G2 = 14;
+        public const int H2 = 15;
+
+        public const int A3 = 16;
+        public const int B3 = 17;
+        public const int C3 = 18;
+        public const int D3 = 19;
+        public const int E3 = 20;
+        public const int F3 = 21;
+        public const int G3 = 22;
+        public const int H3 = 23;
+
+        public const int A4 = 24;
+        public const int B4 = 25;
+        public const int C4 = 26;
+        public const int D4 = 27;
+        public const int E4 = 28;
+        public const int F4 = 29;
+        public const int G4 = 30;
+        public const int H4 = 31;
+
+        public const int A5 = 32;
+        public const int B5 = 33;
+        public const int C5 = 34;
+        public const int D5 = 35;
+        public const int E5 = 36;
+        public const int F5 = 37;
+        public const int G5 = 38;
+        public const int H5 = 39;
+
+        public const int A6 = 40;
+        public const int B6 = 41;
+        public const int C6 = 42;
+        public const int D6 = 43;
+        public const int E6 = 44;
+        public const int F6 = 45;
+        public const int G6 = 46;
+        public const int H6 = 47;
+
+        public const int A7 = 48;
+        public const int B7 = 49;
+        public const int C7 = 50;
+        public const int D7 = 51;
+        public const int E7 = 52;
+        public const int F7 = 53;
+        public const int G7 = 54;
+        public const int H7 = 55;
+
+        public const int A8 = 56;
+        public const int B8 = 57;
+        public const int C8 = 58;
+        public const int D8 = 59;
+        public const int E8 = 60;
+        public const int F8 = 61;
+        public const int G8 = 62;
+        public const int H8 = 63;
+
+        public const int Pawn = 0;
+        public const int Knight = 1;
+        public const int Bishop = 2;
+        public const int Rook = 3;
+        public const int Queen = 3;
+        public const int King = 5;
+
+        public const int White = 0;
+        public const int Black = 1;
+    }
+
+    public class Bitboards
+    {
+        // todo these are Pieces[2][7]
+        public Bitboard Bitboard_WhitePawn { get; set; }
+        public Bitboard Bitboard_WhiteRook { get; set; }
+        public Bitboard Bitboard_WhiteKnight { get; set; }
+        public Bitboard Bitboard_WhiteBishop { get; set; }
+        public Bitboard Bitboard_WhiteQueen { get; set; }
+        public Bitboard Bitboard_WhiteKing { get; set; }
+
+        public Bitboard Bitboard_BlackPawn { get; set; }
+        public Bitboard Bitboard_BlackRook { get; set; }
+        public Bitboard Bitboard_BlackKnight { get; set; }
+        public Bitboard Bitboard_BlackBishop { get; set; }
+        public Bitboard Bitboard_BlackQueen { get; set; }
+        public Bitboard Bitboard_BlackKing { get; set; }
+
+        public Bitboard[,] BetweenMatrix { get; set; }
+
+        public int[] Row { get; set; }
+        public int[] Col { get; set; }
+        public int[] DiagNO { get; set; }
+        public int[] DiagNW { get; set; }
+
+        public Bitboards()
+        {
+            InitBitboard();
+            SetBetweenCube();
+        }        
+
+        private void InitBitboard()
+        {
+            Bitboard_WhitePawn = ConvertToUInt64(new byte[64]
+            {// a1
+                0, 0, 0, 0, 0, 0, 0, 0,
+                1, 1, 1, 1, 1, 1, 1, 1,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0
+            });                   // h8
+
+            Bitboard_WhiteRook = ConvertToUInt64(new byte[64]
+            {
+                1, 0, 0, 0, 0, 0, 0, 1,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0
+            });
+
+            Bitboard_WhiteKnight = ConvertToUInt64(new byte[64]
+            {
+                0, 1, 0, 0, 0, 0, 1, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+            });
+
+            Bitboard_WhiteBishop = ConvertToUInt64(new byte[64]
+            {
+                0, 0, 1, 0, 0, 1, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0
+            });
+
+            Bitboard_WhiteQueen = ConvertToUInt64(new byte[64]
+            {
+                0, 0, 0, 1, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0
+            });
+
+            Bitboard_WhiteKing = ConvertToUInt64(new byte[64]
+            {
+                0, 0, 0, 0, 1, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0
+            });
+
+            Bitboard_BlackPawn = ConvertToUInt64(new byte[64]
+            {
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                1, 1, 1, 1, 1, 1, 1, 1,
+                0, 0, 0, 0, 0, 0, 0, 0
+            });
+
+            Bitboard_BlackRook = ConvertToUInt64(new byte[64]
+            {
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                1, 0, 0, 0, 0, 0, 0, 1
+            });
+
+            Bitboard_BlackKnight = ConvertToUInt64(new byte[64]
+            {
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 1, 0, 0, 0, 0, 1, 0
+            });
+
+            Bitboard_BlackBishop = ConvertToUInt64(new byte[64]
+            {
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 1, 0, 0, 1, 0, 0
+            });
+
+            Bitboard_BlackQueen = ConvertToUInt64(new byte[64]
+            {
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 0
+            });
+
+            Bitboard_BlackKing = ConvertToUInt64(new byte[64]
+            {
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 1, 0, 0, 0
+            });
+
+            Row = new int[64]
+            {
+                0, 0, 0, 0, 0, 0, 0, 0,
+                1, 1, 1, 1, 1, 1, 1, 1,
+                2, 2, 2, 2, 2, 2, 2, 2,
+                3, 3, 3, 3, 3, 3, 3, 3,
+                4, 4, 4, 4, 4, 4, 4, 4,
+                5, 5, 5, 5, 5, 5, 5, 5,
+                6, 6, 6, 6, 6, 6, 6, 6,
+                7, 7, 7, 7, 7, 7, 7, 7,
+            };
+
+            Col = new int[64]
+            {
+                7, 6, 5, 4, 3, 2, 1, 0,
+                7, 6, 5, 4, 3, 2, 1, 0,
+                7, 6, 5, 4, 3, 2, 1, 0,
+                7, 6, 5, 4, 3, 2, 1, 0,
+                7, 6, 5, 4, 3, 2, 1, 0,
+                7, 6, 5, 4, 3, 2, 1, 0,
+                7, 6, 5, 4, 3, 2, 1, 0,
+                7, 6, 5, 4, 3, 2, 1, 0,
+            };
+
+            DiagNO = new int[64]
+            {
+                7, 8, 9,10,11,12,13,14,
+                6, 7, 8, 9,10,11,12,13,
+                5, 6, 7, 8, 9,10,11,12,
+                4, 5, 6, 7, 8, 9,10,11,
+                3, 4, 5, 6, 7, 8, 9,10,
+                2, 3, 4, 5, 6, 7, 8, 9,
+                1, 2, 3, 4, 5, 6, 7, 8,
+                0, 1, 2, 3, 4, 5, 6, 7,
+            };
+
+            DiagNW = new int[64]
+            {
+               14,13,12,11,10, 9, 8, 7,
+               13,12,11,10, 9, 8, 7, 6,
+               12,11,10, 9, 8, 7, 6, 5,
+               11,10, 9, 8, 7, 6, 5, 4,
+               10, 9, 8, 7, 6, 5, 4, 3,
+                9, 8, 7, 6, 5, 4, 3, 2,
+                8, 7, 6, 5, 4, 3, 2, 1,
+                7, 6, 5, 4, 3, 2, 1, 0,
+            };
+        }
+
+        private void SetBetweenCube()
+        {
+            BetweenMatrix = new Bitboard[64, 64];
+            for (int y = 0; y < 64; y++)
+            {
+                for (int x = 0; x < 64; x++)
+                {
+                    if (Col[ x ] == Col[ y ])
+                    {
+                        if (y > x)
+                        {
+                            for (int i = x + 8; i < y; i += 8)
+                            {
+                                SetBit(ref BetweenMatrix[x, y], i);
+                            }
+                        }
+                        else
+                        {
+                            for (int i = x - 8; i > y; i -= 8)
+                            {
+                                SetBit(ref BetweenMatrix[x, y], i);
+                            }
+                        }
+                    }
+                    else if (Row[ x ] == Row[ y ])
+                    {
+                        if (y > x)
+                        {
+                            for (int i = x + 1; i < y; i++)
+                            {
+                                SetBit(ref BetweenMatrix[x, y], i);
+                            }
+                        }
+                        else
+                        {
+                            for (int i = x - 1; i > y; i--)
+                            {
+                                SetBit(ref BetweenMatrix[x, y], i);
+                            }
+                        }
+                    }
+                    else if (DiagNW[x] == DiagNW[y])
+                    {
+                        if (y > x)
+                        {
+                            for (int i = x + 7; i < y; i += 7)
+                            {
+                                SetBit(ref BetweenMatrix[x, y], i);
+                            }
+                        }
+                        else
+                        {
+                            for (int i = x - 7; i > y; i -= 7)
+                            {
+                                SetBit(ref BetweenMatrix[x, y], i);
+                            }
+                        }
+                    }
+                    else if (DiagNO[x] == DiagNO[y])
+                    {
+                        if (y > x)
+                        {
+                            for (int i = x + 9; i < y; i += 9)
+                            {
+                                SetBit(ref BetweenMatrix[x, y], i);
+                            }
+                        }
+                        else
+                        {
+                            for (int i = x - 9; i > y; i -= 9)
+                            {
+                                SetBit(ref BetweenMatrix[x, y], i);
+                            }
+                        }
+                    }
+                }
+
+
+            }
+        }
+
+        private static void SetBit(ref UInt64 bitboard, int index)
+        {
+            bitboard |= ((UInt64)0x01 << index);
+        }
+
+        private static void ClearBit(ref UInt64 bitboard, int index)
+        {
+            bitboard &= (~((UInt64)0x01 << index));
+        }
+
+        public static UInt64 ConvertToUInt64(byte[] input)
+        {
+            UInt64 result = 0;
+            for (int i=0; i<64; i++)
+            {
+                if (input[i] != 0)
+                {
+                    SetBit(ref result, i);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MantaChessEngineTest/BitboardTest/BitboardTest.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MantaChessEngine;
+
+namespace MantaChessEngineTest
+{
+    [TestClass]
+    public class BitboardTest
+    {
+        [TestMethod]
+        public void PiecesBitboardTest()
+        {
+            var target = new Bitboards();
+
+            Assert.AreEqual((UInt64)0xff00, target.Bitboard_WhitePawn);
+            Assert.AreEqual((UInt64)0xff000000000000, target.Bitboard_BlackPawn);
+        }
+
+        [TestMethod]
+        public void BetweenMatrixTest()
+        {
+            var target = new Bitboards();
+
+            Assert.AreEqual((UInt64)0x00, target.BetweenMatrix[0, 1]);
+            Assert.AreEqual((UInt64)0x00, target.BetweenMatrix[27, 28]);
+            Assert.AreEqual((UInt64)0x00, target.BetweenMatrix[14, 1]);
+            Assert.AreEqual(Bitboards.ConvertToUInt64(new byte[64]
+            {//(A1)     (D1)
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0
+                //     (D8)         (H8)
+            }), target.BetweenMatrix[Chess.D1, Chess.D8]);
+
+            Assert.AreEqual(Bitboards.ConvertToUInt64(new byte[64]
+            {//(A1)     (D1)
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0
+                //     (D8)         (H8)
+            }), target.BetweenMatrix[Chess.D8, Chess.D1]);
+
+            Assert.AreEqual(Bitboards.ConvertToUInt64(new byte[64]
+            {//(A1)   
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+         /*b3*/ 0, 0, 1, 1, 1, 1, 1, 0, // h3
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0
+                //                  (H8)
+            }), target.BetweenMatrix[Chess.B3, Chess.H3]);
+
+            Assert.AreEqual(Bitboards.ConvertToUInt64(new byte[64]
+            {//(A1)   
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+         /*b3*/ 0, 0, 1, 1, 1, 1, 1, 0, // h3
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0
+                //     (D8)         (H8)
+            }), target.BetweenMatrix[Chess.H3, Chess.B3]);
+
+            Assert.AreEqual(Bitboards.ConvertToUInt64(new byte[64]
+            {//(A1)   
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, // h3
+                0, 0, 0, 0, 0, 0, 1, 0,
+                0, 0, 0, 0, 0, 1, 0, 0,
+                0, 0, 0, 0, 1, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0
+               //       d7         (H8)
+            }), target.BetweenMatrix[Chess.H3, Chess.D7]);
+
+            Assert.AreEqual(Bitboards.ConvertToUInt64(new byte[64]
+            {//(A1)   
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, // h3
+                0, 0, 0, 0, 0, 0, 1, 0,
+                0, 0, 0, 0, 0, 1, 0, 0,
+                0, 0, 0, 0, 1, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0
+               //       d7         (H8)
+            }), target.BetweenMatrix[Chess.D7, Chess.H3]);
+
+            Assert.AreEqual(Bitboards.ConvertToUInt64(new byte[64]
+            {//    b1 
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 1, 0, 0, 0, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 0,
+                0, 0, 0, 0, 1, 0, 0, 0,
+                0, 0, 0, 0, 0, 1, 0, 0,
+                0, 0, 0, 0, 0, 0, 1, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, // h7
+                0, 0, 0, 0, 0, 0, 0, 0
+               //       d7         (H8)
+            }), target.BetweenMatrix[Chess.B1, Chess.H7]);
+
+            Assert.AreEqual(Bitboards.ConvertToUInt64(new byte[64]
+            {//    b1 
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 1, 0, 0, 0, 0, 0,
+                0, 0, 0, 1, 0, 0, 0, 0,
+                0, 0, 0, 0, 1, 0, 0, 0,
+                0, 0, 0, 0, 0, 1, 0, 0,
+                0, 0, 0, 0, 0, 0, 1, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, // h7
+                0, 0, 0, 0, 0, 0, 0, 0
+               //       d7         (H8)
+            }), target.BetweenMatrix[Chess.H7, Chess.B1]);
+        }
+    }
+}

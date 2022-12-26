25d04dfa37fb85fc2c25e6d9ca210d7ef28f165e
huwyss <hansueli.wyss@gmail.com>
2022-12-26 16:49:59 +0100

    Implemented Evaluation for Bitboard.

---
+++ MantaBitboardEngine/Evaluation/BitEvaluator.cs
@@ -0,0 +1,201 @@
+﻿using MantaCommon;
+
+namespace MantaBitboardEngine
+{
+    public class BitEvaluator
+    {
+        private readonly HelperBitboards _helperBits;
+
+        private readonly int[] _value;
+        private readonly int[,,] _positionBonus;
+
+        public BitEvaluator(HelperBitboards helperBits)
+        {
+            _helperBits = helperBits;
+            _value = new int[(int)BitPieceType.Queen + 1];
+            _value[(int)BitPieceType.Pawn] = 100;
+            _value[(int)BitPieceType.Knight] = 300;
+            _value[(int)BitPieceType.Bishop] = 300;
+            _value[(int)BitPieceType.Rook] = 500;
+            _value[(int)BitPieceType.Queen] = 900;
+
+            _positionBonus = new int[2, 6, 64]; // pawn, knight, bishop, rook, queen, king 
+
+            Initialize();
+        }
+
+        public int Evaluate(Bitboards board)
+        {
+            var score = 0;
+            var whiteBishops = 0;
+            var whiteKnights = 0;
+            var blackBishops = 0;
+            var blackKnights = 0;
+
+            for (int color = (int)ChessColor.White; color <= (int)ChessColor.Black; color++)
+            {
+                for (int piece = (int)BitPieceType.Pawn; piece <= (int)BitPieceType.Queen; piece++)
+                {
+                    var whitePieceBit = board.Bitboard_Pieces[color, piece];
+                    while (whitePieceBit != 0)
+                    {
+                        var square = BitHelper.BitScanForward(whitePieceBit);
+                        whitePieceBit &= _helperBits.NotIndexMask[square];
+
+                        if (color == (int)ChessColor.White)
+                        {
+                            score += _value[piece] + _positionBonus[color, piece, square];
+                        }
+                        else
+                        {
+                            score -= _value[piece] + _positionBonus[color, piece, square];
+                        }
+
+                        if (piece == (int)BitPieceType.Knight)
+                        {
+                            if (color == (int)ChessColor.White)
+                            {
+                                whiteKnights++;
+                            }
+                            else
+                            {
+                                blackKnights++;
+                            }
+                        }
+                        else if (piece == (int)BitPieceType.Bishop)
+                        {
+                            if (color == (int)ChessColor.White)
+                            {
+                                whiteBishops++;
+                            }
+                            else
+                            {
+                                blackBishops++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if ((whiteBishops == 2 && whiteKnights == 1 && blackBishops == 1 && blackKnights == 2) ||
+                (whiteBishops == 2 && whiteKnights == 0 && blackBishops == 1 && blackKnights == 1) ||
+                (whiteBishops == 2 && whiteKnights == 0 && blackBishops == 0 && blackKnights == 2))
+            {
+                score += 10;
+            }
+            else if ((whiteBishops == 1 && whiteKnights == 2 && blackBishops == 2 && blackKnights == 1) ||
+                    (whiteBishops == 1 && whiteKnights == 1 && blackBishops == 2 && blackKnights == 0) ||
+                    (whiteBishops == 0 && whiteKnights == 2 && blackBishops == 2 && blackKnights == 0))
+            {
+                score -= 10;
+            }
+
+            return score;
+        }
+
+        private void Initialize()
+        {
+            for (var i = 0; i < 64; i++)
+            {
+                _positionBonus[(int)ChessColor.White, (int)BitPieceType.Pawn, i] = PawnPositionBonus[i];
+                _positionBonus[(int)ChessColor.Black, (int)BitPieceType.Pawn, i] = PawnPositionBonus[i];
+
+                _positionBonus[(int)ChessColor.White, (int)BitPieceType.Knight, i] = KnightPositionBonus[i];
+                _positionBonus[(int)ChessColor.Black, (int)BitPieceType.Knight, i] = KnightPositionBonus[i];
+
+                _positionBonus[(int)ChessColor.White, (int)BitPieceType.Bishop, i] = BishopPositionBonus[i];
+                _positionBonus[(int)ChessColor.Black, (int)BitPieceType.Bishop, i] = BishopPositionBonus[i];
+
+                _positionBonus[(int)ChessColor.White, (int)BitPieceType.Rook, i] = RookPositionBonus[63 - i];
+                _positionBonus[(int)ChessColor.Black, (int)BitPieceType.Rook, i] = RookPositionBonus[i];
+
+                _positionBonus[(int)ChessColor.White, (int)BitPieceType.Queen, i] = QueenPositionBonus[63 - i];
+                _positionBonus[(int)ChessColor.Black, (int)BitPieceType.Queen, i] = QueenPositionBonus[i];
+
+                _positionBonus[(int)ChessColor.White, (int)BitPieceType.King, i] = KingPositionBonus[63 - i];
+                _positionBonus[(int)ChessColor.Black, (int)BitPieceType.King, i] = KingPositionBonus[i];
+            }
+        }
+
+        private readonly int[] PawnPositionBonus = new int[64] // pawn in center are more valuable (at least in opening)
+        {
+            0,  0,  0,  0,  0,  0,  0,  0,
+            0,  0,  0,  0,  0,  0,  0,  0,
+            0,  0,  5, 10, 10,  5,  0,  0,
+            0,  0, 10, 15, 15, 10,  0,  0,
+            0,  0, 10, 15, 15, 10,  0,  0,
+            0,  0,  5, 10, 10,  5,  0,  0,
+            0,  0,  0,  0,  0,  0,  0,  0,
+            0,  0,  0,  0,  0,  0,  0,  0,
+
+        //// 0,  0,  0,  0,  0,  0,  0,  0,   // h
+        ////24, 28, 32, 35, 35, 32, 28, 24,   // g
+        //// 5, 10, 16, 25, 25, 16, 10,  5,   // f
+        //// 4,  8, 14, 19, 19, 14,  8,  4,   // e
+        //// 3,  5,  6, 12, 12,  6,  5,  3,   // d
+        //// 2,  4,  3,  7,  7,  3,  4,  2,   // c
+        //// 1,  2,  3, -8, -9,  3,  2,  1,   // b
+        //// 0,  0,  0,  0,  0,  0,  0,  0    // a
+        };
+
+        private readonly int[] KnightPositionBonus = new int[64] // Der Springer am Rande ist eine Schande
+        {
+            -10, -5, -5, -5, -5, -5, -5, -10,
+             -5,  0,  0,  0,  0,  0,  0,  -5,
+             -5,  0,  5,  5,  5,  5,  0,  -5,
+             -5,  0,  5,  5,  5,  5,  0,  -5,
+             -5,  0,  5,  5,  5,  5,  0,  -5,
+             -5,  0,  5,  5,  5,  5,  0,  -5,
+             -5,  0,  0,  0,  0,  0,  0,  -5,
+            -10, -5, -5, -5, -5, -5, -5, -10
+        };
+
+        private readonly int[] BishopPositionBonus = new int[64]
+        {
+           -8, -8, -6, -6, -6, -6, -8, -8,
+            0,  6,  5,  5,  5,  5,  6,  0,
+            0,  4,  5,  6,  6,  5,  4,  0,
+            4,  6,  8,  8,  8,  8,  6,  4,
+            4,  6,  8,  8,  8,  8,  6,  4,
+            0,  4,  5,  6,  6,  5,  4,  0,
+            0,  6,  5,  5,  5,  5,  6,  0,
+           -8, -8, -6, -6, -6, -6, -8, -8
+        };
+
+        private readonly int[] RookPositionBonus = new int[64]
+        {
+            0,  1,  2,  4,  4,  2,  1,  0,  // h
+            3,  5,  7,  7,  7,  7,  5,  3,  // g
+            3,  4,  5,  6,  6,  5,  4,  3,  // f
+           -2,  0,  4,  5,  5,  4,  0, -2,  // e
+           -4, -2,  3,  4,  4,  3, -2, -4,  // d
+           -6, -2,  3,  4,  4,  3, -2, -6,  // c
+           -6, -2,  4,  4,  4,  4, -2, -6,  // b
+           -4, -2,  2,  4,  4,  2, -2, -4   // a
+        };
+
+        private readonly int[] QueenPositionBonus = new int[64]
+        {
+            0,  0,  0,  0,  0,  0,  0,  0,  // h
+            0,  2,  3,  4,  4,  3,  2,  0,  // g
+            0,  2,  4,  5,  5,  3,  2,  0,  // f
+            0,  2,  4,  8,  8,  4,  2,  0,  // e
+           -1,  0,  3,  6,  6,  3,  0, -1,  // d
+           -1,  0,  2,  3,  3,  2,  0, -1,  // c
+           -2, -1,  0,  0,  0,  0, -1, -2,  // b
+           -3, -2, -1,  0,  0, -1, -2, -3   // a
+        };
+
+        private readonly int[] KingPositionBonus = new int[64]
+        {
+            0,  0,  0,  0,  0,  0,  0,  0,  // h
+            0,  0,  0,  0,  0,  0,  0,  0,  // g
+            0,  0,  0,  0,  0,  0,  0,  0,  // f
+            0,  0,  0,  0,  0,  0,  0,  0,  // e
+            0,  0,  0,  0,  0,  0,  0,  0,  // d
+            0,  0,  0,  0,  0,  0,  0,  0,  // c
+            0,  0,  0,  0,  0,  0,  0,  0,  // b
+            0,  0,  5,  0,  0,  0,  5,  0   // a
+        };
+    }
+}
---
+++ MantaBitboardEngineTest/BitboardTest/EvaluatorPositionTest.cs
@@ -0,0 +1,154 @@
+﻿using MantaBitboardEngine;
+using MantaCommon;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MantaChessEngineTest
+{
+    [TestClass]
+    public class MantaBitboardEngineTest
+    {
+        [TestMethod]
+        public void EvaluateTest_WhenPawnInCenter_Then1_2()
+        {
+            var target = new BitEvaluator(new HelperBitboards());
+            var board = new Bitboards();
+            board.SetPosition("........" +
+                              "........" +
+                              "........" +
+                              "........" +
+                              "....P..." +
+                              "........" +
+                              "........" +
+                              "........");
+
+            var score = target.Evaluate(board);
+
+            Assert.AreEqual(115, score);
+        }
+
+        [TestMethod]
+        public void EvaluateTest_WhenPawnIn3rdRank_Then1_1()
+        {
+            var target = new BitEvaluator(new HelperBitboards());
+            var board = new Bitboards();
+            board.SetPosition("........" +
+                              "........" +
+                              "........" +
+                              "........" +
+                              "........" +
+                              "....P..." +
+                              "........" +
+                              "........");
+
+            var score = target.Evaluate(board);
+
+            Assert.AreEqual(110, score);
+        }
+
+        [TestMethod]
+        public void EvaluateTest_WhenKnightIsAtBorder_ThenItsAShame()
+        {
+            var target = new BitEvaluator(new HelperBitboards());
+            var board = new Bitboards();
+            board.SetPosition("..k..K.." +
+                              "........" +
+                              "........" +
+                              "........" +
+                              "........" +
+                              "........" +
+                              ".......N" +
+                              "........");
+
+            var score = target.Evaluate(board);
+
+            Assert.AreEqual(300 - 5, score);
+        }
+        
+        [TestMethod]
+        public void EvaluateTest_WhenKnightIsNotAtBorder_ThenItsOk()
+        {
+            var target = new BitEvaluator(new HelperBitboards());
+            var board = new Bitboards();
+            board.SetPosition("........" +
+                              "........" +
+                              "........" +
+                              "........" +
+                              "........" +
+                              "........" +
+                              "....N..." +
+                              "........");
+
+            var score = target.Evaluate(board);
+
+            Assert.AreEqual(300, score);
+        }
+
+        [TestMethod]
+        public void EvaluateTest_WhenWhiteHasTwoBishopAndBlackHasBishopAndKnight_ThenWhiteBetter()
+        {
+            var target = new BitEvaluator(new HelperBitboards());
+            var board = new Bitboards();
+            board.SetPosition("........" +
+                              "........" +
+                              "..n..b.." +
+                              "........" +
+                              "........" +
+                              "..B..B.." +
+                              "........" +
+                              "........");
+
+            var score = target.Evaluate(board);
+
+            Assert.AreEqual(true, score > 0.1f, "Two bishops should be better than bishop and knight.");
+        }
+
+        [TestMethod]
+        public void EvaluateTest_WhenBlackHasDoubleBishop_ThenBlackBetter()
+        {
+            var target = new BitEvaluator(new HelperBitboards());
+            var board = new Bitboards();
+            board.SetPosition("........" +
+                              "........" +
+                              "..b..b.." +
+                              "........" +
+                              "........" +
+                              "..N..B.." +
+                              "........" +
+                              "........");
+
+            var score = target.Evaluate(board);
+
+            Assert.AreEqual(true, score < -0.1f, "Two bishops should be better than bishop and knight.");
+        }
+
+        [TestMethod]
+        public void EvaluateTest_WhenWhiteDidCastle_ThenWhiteBetter()
+        {
+            var target = new BitEvaluator(new HelperBitboards());
+            var board = new Bitboards();
+            board.SetPosition("rnbqk..r" +
+                              "pppppppp" +
+                              "........" +
+                              "........" +
+                              "........" +
+                              "........" +
+                              "PPPPPPPP" +
+                              "RNBQK..R");
+
+            // white castling
+            board.Move(BitMove.CreateCastling(ChessColor.White, CastlingType.KingSide, 0));
+            var score = target.Evaluate(board);
+            Assert.AreEqual(true, score > 0.1f, "White did castling. so white should be better.");
+
+            // black castling
+            board.Move(BitMove.CreateCastling(ChessColor.Black, CastlingType.KingSide, 0));
+            score = target.Evaluate(board);
+            Assert.AreEqual(true, score == 0, "White and Black did castling. They are equal.");
+
+            // take black move back
+            board.Back();
+            score = target.Evaluate(board);
+            Assert.AreEqual(true, score > 0.1f, "Black castling was taken back. so white should be better.");
+        }
+    }
+}
